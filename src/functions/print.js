77b0ab92a72deb7ca5d57103ae08ccf4c588d1e1
William Harrison <william@williamdavidharrison.com.au>
2023-01-06 11:50:47 +0800

    v1.0.0
    Initial release of Easy Script

---
+++ src/functions/print.js
@@ -0,0 +1,7 @@
+module.exports = function print(input) {
+    if(!input) {
+        throw new Error("No input specified!");
+    }
+
+    return console.log(input);
+}
---
+++ src/modules.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "log": require("./functions/log"),
+    "print": require("./functions/print")
+}
