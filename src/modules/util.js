3300268d667ffa3e738f62f10001e51747801869
William Harrison <william@williamdavidharrison.com.au>
2023-01-07 15:36:29 +0800

    feat(util): `boolToNum` module

--- src/modules/util.js
+++ src/modules/util.js
@@ -1,4 +1,5 @@
 module.exports = {
+    "boolToNum": require("./util/boolToNum"),
     "boolToStr": require("./util/boolToStr"),
     "numToStr": require("./util/numToStr"),
     "strToBool": require("./util/strToBool"),
---
+++ src/modules/util/boolToNum.js
@@ -0,0 +1,10 @@
+module.exports = function boolToNum(boolean) {
+    if(typeof boolean !== "boolean") throw new Error("No boolean provided");
+
+    let res;
+
+    if(boolean) res = + true;
+    if(!boolean) res = + false;
+
+    return res;
+}
