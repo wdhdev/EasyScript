804adb81c06a841040ba1b0636e5a0a3e2469c1e
William Harrison <william@williamdavidharrison.com.au>
2023-01-07 16:09:47 +0800

    fix(module): error when no input

--- src/modules/random.js
+++ src/modules/random.js
@@ -1,7 +1,7 @@
 module.exports = function random(max) {
     let highest = 100;
 
-    if(isNaN(max)) throw new Error("Max number must be a number");
+    if(max && !isFinite(max)) throw new Error("Max number must be a number");
     if(max && max <= 1 || max && max >= 2147483647) throw new Error("Max number must be higher than 1 and lower than 2,147,483,647");
 
     if(max) highest = max;
