1425eaf82cc5c9ff92d2fa6b2ff7ed84094bb339
William Harrison <william@williamdavidharrison.com.au>
2023-01-12 15:37:04 +0800

    revert `if ()` changes

--- src/modules/random.js
+++ src/modules/random.js
@@ -7,10 +7,10 @@
 function random(max) {
     let highest = 100;
 
-    if (max && !isFinite(max)) throw new Error("Max number must be a number");
-    if (max && max <= 1 || max && max >= 2147483647) throw new Error("Max number must be higher than 1 and lower than 2,147,483,647");
+    if(max && !isFinite(max)) throw new Error("Max number must be a number");
+    if(max && max <= 1 || max && max >= 2147483647) throw new Error("Max number must be higher than 1 and lower than 2,147,483,647");
 
-    if (max) highest = max;
+    if(max) highest = max;
 
     const result = Math.floor((Math.random() * highest) + 1);
 
