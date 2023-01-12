3387f0b4cec0b3ff7a2b9953ad2de2908f6564a2
William Harrison <william@williamdavidharrison.com.au>
2023-01-12 15:34:51 +0800

    add extra space

--- src/modules/flip.js
+++ src/modules/flip.js
@@ -3,6 +3,7 @@
  * @function `flip` - Quickly execute a heads or tails decision randomly 
  * @returns {string} Returns "heads" or "tails" (*as string*)
  */
+
 function flip() {
     return Math.random() >= 0.5 ? "heads" : "tails";
 }
