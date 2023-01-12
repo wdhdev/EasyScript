b230e1c33cca766aee60f7c854af5e8c4f49ccf1
William Harrison <william@williamdavidharrison.com.au>
2023-01-12 15:35:46 +0800

    revert `if ()` changes + remove extra space + add space

--- src/modules/id.js
+++ src/modules/id.js
@@ -1,5 +1,5 @@
-
 const { customAlphabet } = require("nanoid");
+
 /**
  * @function `id` - Randomly generate a unique ID composed of random characters and numbers 
  * @param {Number} length - (*Required*) The length of the ID. Condition: *`1 <= length <= 64`*
@@ -7,10 +7,10 @@ const { customAlphabet } = require("nanoid");
  */
 
 function id(length) {
-    if (!length) throw new Error("No length specified");
-    if (typeof length !== "number") throw new Error("Invalid type specified");
+    if(!length) throw new Error("No length specified");
+    if(typeof length !== "number") throw new Error("Invalid type specified");
 
-    if (length > 64) throw new Error("Length cannot be more than 64");
+    if(length > 64) throw new Error("Length cannot be more than 64");
 
     const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", length);
     const id = nanoid();
