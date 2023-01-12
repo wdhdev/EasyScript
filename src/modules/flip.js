4c5580da8e7bc2967b464e4edd25e9acf2e2ebb9
Abdennour Boudjema <nourik27@gmail.com>
2023-01-12 04:22:22 +0100

    added self documentation

--- src/modules/flip.js
+++ src/modules/flip.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @function `flip` - Quickly execute a heads or tails decision randomly 
+ * @returns {string} Returns "heads" or "tails" (*as string*)
+ */
 function flip() {
     return Math.random() >= 0.5 ? "heads" : "tails";
 }
