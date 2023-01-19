76f11ee8d6a437e4a71104cec6211872f4d698c9
William Harrison <william@williamdavidharrison.com.au>
2023-01-19 17:48:25 +0800

    feat: move `modules.js` into `index.js`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const modules = require("./modules");
+const modules = {
+    flip: require("./modules/flip"),
+    id: require("./modules/id"),
+    log: require("./modules/log"),
+    print: require("./modules/print"),
+    random: require("./modules/random"),
+    util: {
+        boolToNum: require("./modules/util/boolToNum"),
+        boolToStr: require("./modules/util/boolToStr"),
+        numToStr: require("./modules/util/numToStr"),
+        strToBool: require("./modules/util/strToBool"),
+        strToNum: require("./modules/util/strToNum"),
+        removeDuplicates: require("./modules/util/removeDuplicates")
+    },
+    uuid: require("./modules/uuid")
+}
 
-module.exports = modules;
+module.exports = { modules };
