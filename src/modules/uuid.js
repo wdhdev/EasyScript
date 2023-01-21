10e47ed6755dc58aae676993bd60acf648d29f5d
William Harrison <william@williamdavidharrison.com.au>
2023-01-21 16:57:18 +0800

    feat(uuid): return string if 1 uuid

--- src/modules/uuid.js
+++ src/modules/uuid.js
@@ -1,11 +1,12 @@
 /**
- * @function `uuid` - Generate a unique UUID composed of random characters and numbers.
+ * @function `uuid` - Generate a unique UUID composed of random characters and numbers. UUIDs follow this rule: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
  * @param { Number } [number] - The amount of UUID's to generate. Condition: **`1 < amount < 64`**
- * @returns { Array } - An array of unique UUID's of the amount inputted that follows this rule: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+ * @returns { Array } - Returns an array if the amount inputted is above 1.
+ * @returns { String } - Returns a string if the amount inputted is equal to 1.
  */
 
 function generateUUID(amount) {
-    const uuids = [];
+    let uuids = [];
 
     let i = 0;
 
@@ -28,6 +29,8 @@ function generateUUID(amount) {
 
         i++;
     } while (i < amount);
+
+    if (uuids.length === 1) uuids = uuids[0];
 
     return uuids;
 }
