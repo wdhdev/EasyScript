f15ca5a14dc42617e6d545fd756450ff599c5c4b
William Harrison <william@williamdavidharrison.com.au>
2023-01-25 16:13:23 +0800

    feat: add multi-id generation

--- src/modules/id.js
+++ src/modules/id.js
@@ -2,21 +2,43 @@ const { customAlphabet } = require("nanoid");
 
 /**
  * @function `id` - Generate a unique ID composed of random characters and numbers.
- * @param { Number } length - *Required* The length of the ID.
- * @returns { String } - A unique ID string of a specified length (e.g. abthvmyt3h1j3it)
+ * @param { Number } [length] - The length of each ID.
+ * @param { Number } [amount] - The amount of IDs to generate. Condition: **`1 < amount < 64`**
+ * @returns { Array } - Returns an array if the amount inputted is above 1.
+ * @returns { String } - Returns a string if the amount inputted is equal to 1.
  */
 
-function id(length) {
+function generateId(length, amount) {
+    let ids = [];
+
+    let i = 0;
+
+    do {
+        const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", length );
+        const id = nanoid();
+
+        ids.push(id);
+
+        i++;
+    } while (i < amount);
+
+    if (ids.length === 1) ids = ids[0];
+
+    return ids;
+}
+
+function id(length, amount) {
     if (!length) throw new SyntaxError("No length specified");
-    if (typeof length !== "number") throw new TypeError("Invalid type specified");
+    if (typeof length !== "number") throw new TypeError("Length must be Number type");
 
-    const nanoid = customAlphabet(
-        "1234567890abcdefghijklmnopqrstuvwxyz",
-        length
-    );
-    const id = nanoid();
+    let ids = 1;
 
-    return id;
+    if (amount && !isFinite(amount)) throw new TypeError("Amount must be Number type");
+    if ((amount && amount < 1) || (amount && amount >= 64)) throw new RangeError("Amount must be equal or higher than 1 and lower than 64");
+
+    if (amount) ids = amount;
+
+    return generateId(length, amount);
 }
 
 module.exports = id;
