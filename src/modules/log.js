9e384b000286905da09aef58ce4ad0373633a99b
William Harrison <william@williamdavidharrison.com.au>
2023-02-04 14:49:01 +0800

    feat: minimize code

--- src/modules/log.js
+++ src/modules/log.js
@@ -4,10 +4,8 @@
  * @returns { void } - An instance of console.log() that is executed using `input`.
  */
 
-function log(input) {
+module.exports = function log(input) {
     if (!input) throw new SyntaxError("No input specified");
 
     return console.log(input);
 }
-
-module.exports = log;
