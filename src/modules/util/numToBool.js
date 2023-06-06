12cb90961d39d503254de646366cc6acdc101daa
Xiro <creaperhunter531@gmail.com>
2023-06-06 19:28:13 +0200

    Code improvement

--- src/modules/util/numToBool.js
+++ src/modules/util/numToBool.js
@@ -8,9 +8,5 @@ module.exports = function numToBool(number) {
     if (typeof number !== "number") throw new TypeError("No number provided");
     if (number !== 1 && number !== 0) throw new SyntaxError("Number must be either 0 or 1");
 
-    let res = true;
-
-    if (number === 0) res = false;
-
-    return res;
+    return number === 1;
 }
