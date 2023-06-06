5872d84c9078960c6f7481210d42a2abeb71880a
Xiro <creaperhunter531@gmail.com>
2023-06-06 19:42:48 +0200

    Added min param and removed useless code

--- src/modules/random.js
+++ src/modules/random.js
@@ -1,18 +1,14 @@
 /**
  * @function `random` - Used to generate a random number between 1 and a max value.
- * @param { Number } [max] - The max value used. Condition: **`1 < max < 2_147_483_647`**
+ * @param { Number } [min] - The min value used. 
+ * @param { Number } [max] - The max value used.
  * @returns { Number } - A random number between 1 and max (*parameter*). If max is *undefined*, will return a random number between 1 and 100.
  */
 
-module.exports = function random(max) {
-    let highest = 100;
+module.exports = function random(min, max) {
+    if (max && !isFinite(max) || min && !isFinite(min)) throw new TypeError("Parameter number must be a number");
+    if (min >= max) throw new RangeError("Parameter min must be less than max");
+    else if (min <= 0 && min >= max) throw new RangeError("Parameter max must be greater than min");
 
-    if (max && !isFinite(max)) throw new TypeError("Max number must be a number");
-    if ((max && max <= 1) || (max && max >= 2147483647)) throw new RangeError("Max number must be higher than 1 and lower than 2,147,483,647");
-
-    if (max) highest = max;
-
-    const result = Math.floor(Math.random() * highest + 1);
-
-    return result;
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
