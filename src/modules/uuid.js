c7607b92856662e3535b6bd1fc1edcfeb0a158bf
William Harrison <william@williamdavidharrison.com.au>
2023-06-09 14:30:52 +0800

    Update uuid.js

--- src/modules/uuid.js
+++ src/modules/uuid.js
@@ -5,26 +5,22 @@
  */
 
 module.exports = function uuid(number) {
-  let amount = 1;
+    let amount = 1;
 
-  if (number && !isFinite(number))
-    throw new TypeError('Max number must be a number');
-  if ((number && number < 1) || (number && number >= 64))
-    throw new RangeError(
-      'Amount must be equal or higher than 1 and lower than 64'
-    );
+    if (number && !isFinite(number)) throw new TypeError("Max number must be a number");
+    if ((number && number < 1) || (number && number >= 64)) throw new RangeError("Amount must be equal or higher than 1 and lower than 64");
 
-  if (number) amount = number;
+    if (number) amount = number;
 
-  let uuids = [];
+    let uuids = [];
 
-  for (var i = 0; i < amount; i++) {
-    const uuid = require('crypto').randomUUID();
+    for (var i = 0; i < amount; i++) {
+        const uuid = require("crypto").randomUUID();
 
-    uuids.push(uuid);
-  }
+        uuids.push(uuid);
+    }
 
-  if (uuids.length === 1) return uuids[0];
+    if (uuids.length === 1) return uuids[0];
 
-  return uuids;
+    return uuids;
 }
